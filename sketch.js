bcd68149d072ba0f6a72bea63f00f7b07fa564c4
AnayaJohn <64412959+AnayaJohn@users.noreply.github.com>
2020-07-08 09:23:51 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,62 @@
+//var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+//var packageBody,ground
+
+var ground, paper;
+var dustbin2,dustbin1;
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+	
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	ground= new Ground(400,700,800,20);
+	paper= new Paper(300,640,20);
+	dustbin1= new Ground(600,640,20,100);
+	dustbin2= new Ground(700,640,20,100);
+	dustbinImg= new Dustbin(400,400);
+	
+
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(200);
+
+  ground.display();
+  paper.display();
+  dustbin1.display();
+  dustbin2.display();
+  dustbinImg.display();
+  
+
+  
+  
+  drawSprites();
+ 
+}
+function keyPressed(){
+	if(keyCode=== UP_ARROW){
+		Matter.Body.applyForce(paper.body,paper.body.position,{x:40,y:-55});
+	}
+}
+
+
+
+
